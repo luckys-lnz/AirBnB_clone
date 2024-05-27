fec608ad4bb38a0593c2029a152a3e271f6b2627
luckys-lnz <lalucky22@gmail.com>
2024-05-27 22:41:09 +0000

    updated the file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines the FileStorage class."""
+import os
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -25,24 +26,28 @@ class FileStorage:
 
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id"""
-        ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        obj_class = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(obj_class, obj.id)] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
-        odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
+        obj_storage = FileStorage.__objects
+        object_dict = {obj: obj_storage[obj].to_dict()
+                       for obj in obj_storage.keys()}
+        with open(FileStorage.__file_path, "w") as file:
+            json.dump(object_dict, file)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """
+        Deserialize the JSON file
+        __file_path to __objects, if it exists.
+        """
         try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
+            with open(FileStorage.__file_path) as file:
+                objdict = json.load(file)
+                for obj in objdict.values():
+                    cls_name = obj["__class__"]
+                    del obj["__class__"]
+                    self.new(eval(cls_name)(**obj))
         except FileNotFoundError:
             return
